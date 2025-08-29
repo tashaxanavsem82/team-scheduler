b04fde1c23b5e29452d2b64e7a016947896b3abe
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-28 21:15:21 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Team Scheduler</h1>
+      <p>Welcome to the Team Scheduler Application.</p>
+    </div>
+  );
+};
+
+export default App;
