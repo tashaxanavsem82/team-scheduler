ef8f4b7705247962b8aacd850e74e1fa21e42cd2
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-28 21:15:18 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
